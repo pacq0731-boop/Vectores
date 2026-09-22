17781744cd840af9b709cd5172ac6422a424cc36
Paula Castañeda <pacq0731@gmail.com>
2026-09-21 20:18:18 -0500

    Primer commit

---
+++ src/main/java/com/mycompany/vectores/Vectores.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.vectores;
+
+/**
+ *
+ * @author User
+ */
+public class Vectores {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
